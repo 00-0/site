a1d51ca1f8eee025b8bfbb1d421349c8955fb861
paulomouat <paulo.mouat@gmail.com>
2018-04-05 12:04:15 -0400

    Added update script.

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git checkout master
+git pull
+git add -A
+git commit
+git push
+git checkout gh-pages
+git pull
+git merge -m "Merged latest from master" master
+git push
+git checkout master
